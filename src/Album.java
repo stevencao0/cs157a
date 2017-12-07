6df44f55bed95e2de2c90017b4b16344144f11d8
stevencao0 <stevencao0@users.noreply.github.com>
2017-12-07 12:05:30 -0800

    Add files via upload

---
+++ src/Album.java
@@ -0,0 +1,63 @@
+
+public class Album {
+
+	private int albumID;
+	private String albumArtist;
+	private String albumName;
+	private int albumRating;
+	private int totalScore;
+	private int ratingAmt;
+	private String albumDate;
+	private String albumGenre;
+	
+	public String getAlbumDate() {
+		return albumDate;
+	}
+	public void setAlbumDate(String albumDate) {
+		this.albumDate = albumDate;
+	}
+	public String getAlbumGenre() {
+		return albumGenre;
+	}
+	public void setAlbumGenre(String albumGenre) {
+		this.albumGenre = albumGenre;
+	}
+	public int getAlbumID() {
+		return albumID;
+	}
+	public void setAlbumID(int albumID) {
+		this.albumID = albumID;
+	}
+	public String getAlbumArtist() {
+		return albumArtist;
+	}
+	public void setAlbumArtist(String albumArtist) {
+		this.albumArtist = albumArtist;
+	}
+	public String getAlbumName() {
+		return albumName;
+	}
+	public void setAlbumName(String albumName) {
+		this.albumName = albumName;
+	}
+	public int getAlbumRating() {
+		return albumRating;
+	}
+	public void setAlbumRating(int albumRating) {
+		this.albumRating = albumRating;
+	}
+	public int getRatingAmt() {
+		return ratingAmt;
+	}
+	public void setRatingAmt(int ratingAmt) {
+		this.ratingAmt = ratingAmt;
+	}
+	public int getTotalScore() {
+		return totalScore;
+	}
+	public void setTotalScore(int totalScore) {
+		this.totalScore = totalScore;
+	}
+
+	
+}
